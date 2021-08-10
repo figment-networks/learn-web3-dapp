14e9a0624dfefabf9d071b0ca03a3c5899ade22c
Rogan X <rogan@figment.io>
2021-08-10 16:52:47 -0600

    More Prettifier

--- components/protocols/avalanche/steps/Query.tsx
+++ components/protocols/avalanche/steps/Query.tsx
@@ -1,40 +1,40 @@
-import { useState } from "react";
-import { Button, Space, Col, Typography } from "antd";
-import axios from "axios";
-import { LoadingOutlined } from "@ant-design/icons";
+import { useState } from 'react'
+import { Button, Space, Col, Typography } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import axios from 'axios'
 
-import { AvalancheQueryResponse } from "types/avalanche-types";
-import styled from "styled-components";
+import { AvalancheQueryResponse } from '@avax/types'
+import styled from 'styled-components'
 
-const { Text } = Typography;
+const { Text } = Typography
 
 const Query = () => {
   const [queryData, setQueryData] = useState<AvalancheQueryResponse | null>(
     null
-  );
-  const [fetching, setFetching] = useState<boolean>(false);
+  )
+  const [fetching, setFetching] = useState<boolean>(false)
 
   const getQuery = () => {
-    setFetching(true);
+    setFetching(true)
     axios
       .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/avalanche/query`)
       .then((res) => {
-        const data: AvalancheQueryResponse = res.data;
-        setQueryData(data);
-        setFetching(false);
+        const data: AvalancheQueryResponse = res.data
+        setQueryData(data)
+        setFetching(false)
       })
       .catch((err) => {
-        console.log(err);
-        setFetching(false);
-      });
-  };
+        console.log(err)
+        setFetching(false)
+      })
+  }
 
   return (
     <Col>
-      <Space direction="vertical" size="large">
-        <Space direction="vertical">
+      <Space direction='vertical' size='large'>
+        <Space direction='vertical'>
           <Text>We can query information from Avalanche using the InfoAPI</Text>
-          <Button type="primary" onClick={getQuery}>
+          <Button type='primary' onClick={getQuery}>
             Query the network
           </Button>
           {fetching ? (
@@ -45,14 +45,14 @@ const Query = () => {
         </Space>
       </Space>
     </Col>
-  );
-};
+  )
+}
 
 const Code = styled.pre`
   font-size: 0.9em;
   padding: 10px;
   background-color: #eee;
   margin-top: 20px;
-`;
+`
 
-export default Query;
+export default Query
